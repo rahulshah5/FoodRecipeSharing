e450d4c2dee2363400d95f14c48ab8806759ac53
mrcoder <mrcoder2057@gmail.com>
2024-10-23 14:37:29 +0545

    fixes

--- FoodRecipeSharingApp/models.py
+++ FoodRecipeSharingApp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
-from django.db import models
 from django.db.models import Avg
 
 class CustomUserManager(BaseUserManager):
